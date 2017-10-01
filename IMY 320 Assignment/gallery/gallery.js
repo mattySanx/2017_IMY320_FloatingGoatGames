de1742ef190be0e8634475fe27f949f0ab1a07c4
Anneta Wamono <annetawamono@yahoo.com.au>
2017-10-01 17:28:01 +0200

    Trying to fix the gallery

--- IMY 320 Assignment/gallery/gallery.js
+++ IMY 320 Assignment/gallery/gallery.js
@@ -9,156 +9,224 @@ var HEIGHT = document.getElementById('world').offsetHeight;
 window.addEventListener('load', init, false);
 
 function init() {
-	american = true;
-	russian = false;
-	cuban = false;
+  american = true;
+  russian = false;
+  cuban = false;
 
-	select = 1;
+  select = 0;
 
-	bioAmerican = document.getElementById('american');
-	bioRussian = document.getElementById('russian');
-	bioCuban = document.getElementById('cuban');
+  bioAmerican = document.getElementById('american');
+  bioRussian = document.getElementById('russian');
+  bioCuban = document.getElementById('cuban');
+	leftBtn = document.getElementById("leftBtn");
+	rightBtn = document.getElementById("rightBtn");
 
-	initScene();
-	initCamera();
-	initLights();
-	initRenderer();
+  initScene();
+  initCamera();
+  initLights();
+  initRenderer();
 
-	container = document.getElementById('world');
-	container.appendChild(renderer.domElement);
+  container = document.getElementById('world');
+  container.appendChild(renderer.domElement);
 
-	initModel();
+  initModel();
 
-	document.addEventListener('mousemove', handleMouseMove, false);
+  document.addEventListener('mousemove', handleMouseMove, false);
 
-	render();
+  render();
 }
 
-var mousePos = {x: 0, y:0};
+var mousePos = {
+  x: 0,
+  y: 0
+};
 
 function handleMouseMove(event) {
-	//note to self, change width and height to that of the entire window
-	var tx = -1 + (event.clientX/window.innerWidth)*2;
-	var ty = 1 - (event.clientY/window.innerHeight)*2;
+  //note to self, change width and height to that of the entire window
+  var tx = -1 + (event.clientX / window.innerWidth) * 2;
+  var ty = 1 - (event.clientY / window.innerHeight) * 2;
 
-	mousePos = {x: tx, y:ty};
+  mousePos = {
+    x: tx,
+    y: ty
+  };
 }
 
 function initScene() {
-	scene = new THREE.Scene();	
+  scene = new THREE.Scene();
 }
 
 function initCamera() {
-	camera = new THREE.PerspectiveCamera(70, WIDTH/HEIGHT, 1, 13);
-	camera.position.set(0, 3, 5);
-	camera.lookAt(scene.position);
+  camera = new THREE.PerspectiveCamera(70, WIDTH / HEIGHT, 1, 13);
+  camera.position.set(0, 3, 5);
+  camera.lookAt(scene.position);
 }
 
 function initRenderer() {
-	renderer = new THREE.WebGLRenderer({ alpha:true, antialias:true });
-	renderer.setSize(WIDTH, HEIGHT);
+  renderer = new THREE.WebGLRenderer({
+    alpha: true,
+    antialias: true
+  });
+  renderer.setSize(WIDTH, HEIGHT);
 }
 
 var mesh = null;
+
 function initModel() {
-	var loader = new THREE.JSONLoader();
-	if(american) {
-		loader.load('american/characterfixednormals.json', function(geometry, materials) {
-			var material1 = new THREE.MeshPhongMaterial({map: new THREE.TextureLoader().load('american/apparel.png'), side: THREE.DoubleSide});
-			var material2 = new THREE.MeshPhongMaterial({map: new THREE.TextureLoader().load('american/head.png'), side: THREE.DoubleSide});
-			materials2 = [material2, material1, materials[2]];
-			mesh = new THREE.Mesh(geometry, materials2);
-			mesh.scale.set(0.5, 0.5, 0.5);
-			mesh.position.y -= 5;
-			scene.add(mesh);
-		});	
-	}
+  var loader = new THREE.JSONLoader();
+  if (american) {
+    loader.load('american/characterfixednormals.json', function(geometry, materials) {
+      var material1 = new THREE.MeshPhongMaterial({
+        map: new THREE.TextureLoader().load('american/apparel.png'),
+        side: THREE.DoubleSide
+      });
+      var material2 = new THREE.MeshPhongMaterial({
+        map: new THREE.TextureLoader().load('american/head.png'),
+        side: THREE.DoubleSide
+      });
+      materials2 = [material2, material1, materials[2]];
+      mesh = new THREE.Mesh(geometry, materials2);
+      mesh.scale.set(0.5, 0.5, 0.5);
+      mesh.position.y -= 5;
+      scene.add(mesh);
+    });
+  }
 
-	if(russian) {
-		loader.load('russian/russianRIGIFIFUCKUP.json', function(geometry, materials) {
-			var material1 = new THREE.MeshPhongMaterial({map: new THREE.TextureLoader().load('russian/coat.png'), side: THREE.DoubleSide});
-			var material2 = new THREE.MeshPhongMaterial({map: new THREE.TextureLoader().load('russian/gloves.001.png'), side: THREE.DoubleSide});
-			var material3 = new THREE.MeshPhongMaterial({map: new THREE.TextureLoader().load('russian/apparel.png'), side: THREE.DoubleSide});
-			var material4 = new THREE.MeshPhongMaterial({map: new THREE.TextureLoader().load('russian/pants.png'), side: THREE.DoubleSide});
-			var material5 = new THREE.MeshPhongMaterial({map: new THREE.TextureLoader().load('russian/face.png'), side: THREE.DoubleSide});
-			var material6 = new THREE.MeshPhongMaterial({map: new THREE.TextureLoader().load('russian/hat.png'), side: THREE.DoubleSide});
-			
-			var mats = [material1, material2, material3, material4, material5, material6];
-			mesh = new THREE.Mesh(geometry, mats);
-			mesh.scale.set(0.4, 0.4, 0.4);
-			mesh.position.set(0, -6, -1);
-			scene.add(mesh);
-		});
-	}
+  if (russian) {
+    loader.load('russian/russianRIGIFIFUCKUP.json', function(geometry, materials) {
+      var material1 = new THREE.MeshPhongMaterial({
+        map: new THREE.TextureLoader().load('russian/coat.png'),
+        side: THREE.DoubleSide
+      });
+      var material2 = new THREE.MeshPhongMaterial({
+        map: new THREE.TextureLoader().load('russian/gloves.001.png'),
+        side: THREE.DoubleSide
+      });
+      var material3 = new THREE.MeshPhongMaterial({
+        map: new THREE.TextureLoader().load('russian/apparel.png'),
+        side: THREE.DoubleSide
+      });
+      var material4 = new THREE.MeshPhongMaterial({
+        map: new THREE.TextureLoader().load('russian/pants.png'),
+        side: THREE.DoubleSide
+      });
+      var material5 = new THREE.MeshPhongMaterial({
+        map: new THREE.TextureLoader().load('russian/face.png'),
+        side: THREE.DoubleSide
+      });
+      var material6 = new THREE.MeshPhongMaterial({
+        map: new THREE.TextureLoader().load('russian/hat.png'),
+        side: THREE.DoubleSide
+      });
 
-	if(cuban) {
-		loader.load('cuban/cuban3.json', function(geometry, materials) {
-			//var material1 = new THREE.MeshPhongMaterial({map: new THREE.TextureLoader().load('cuban/coat.png'), side: THREE.DoubleSide});
-			//var material2 = new THREE.MeshPhongMaterial({map: new THREE.TextureLoader().load('cuban/gloves.001.png'), side: THREE.DoubleSide});
-			var material3 = new THREE.MeshPhongMaterial({map: new THREE.TextureLoader().load('cuban/PANTS.png'), side: THREE.DoubleSide});
-			var material4 = new THREE.MeshPhongMaterial({map: new THREE.TextureLoader().load('cuban/BELT.png'), side: THREE.DoubleSide});
-			var material5 = new THREE.MeshPhongMaterial({map: new THREE.TextureLoader().load('cuban/SHIRT.png'), side: THREE.DoubleSide});
-			var material6 = new THREE.MeshPhongMaterial({map: new THREE.TextureLoader().load('cuban/cubanHead.001.png'), side: THREE.DoubleSide});
-			
-			var mats = [materials[0], materials[0], material3, material4, material5, material6];
-			mesh = new THREE.Mesh(geometry, mats);
-			mesh.scale.set(0.3, 0.3, 0.3);
-			mesh.position.set(0, -9, 0);
-			scene.add(mesh);
-		});
-	}
+      var mats = [material1, material2, material3, material4, material5, material6];
+      mesh = new THREE.Mesh(geometry, mats);
+      mesh.scale.set(0.4, 0.4, 0.4);
+      mesh.position.set(0, -6, -1);
+      scene.add(mesh);
+    });
+  }
+
+  if (cuban) {
+    loader.load('cuban/cuban3.json', function(geometry, materials) {
+      //var material1 = new THREE.MeshPhongMaterial({map: new THREE.TextureLoader().load('cuban/coat.png'), side: THREE.DoubleSide});
+      //var material2 = new THREE.MeshPhongMaterial({map: new THREE.TextureLoader().load('cuban/gloves.001.png'), side: THREE.DoubleSide});
+      var material3 = new THREE.MeshPhongMaterial({
+        map: new THREE.TextureLoader().load('cuban/PANTS.png'),
+        side: THREE.DoubleSide
+      });
+      var material4 = new THREE.MeshPhongMaterial({
+        map: new THREE.TextureLoader().load('cuban/BELT.png'),
+        side: THREE.DoubleSide
+      });
+      var material5 = new THREE.MeshPhongMaterial({
+        map: new THREE.TextureLoader().load('cuban/SHIRT.png'),
+        side: THREE.DoubleSide
+      });
+      var material6 = new THREE.MeshPhongMaterial({
+        map: new THREE.TextureLoader().load('cuban/cubanHead.001.png'),
+        side: THREE.DoubleSide
+      });
+
+      var mats = [materials[0], materials[0], material3, material4, material5, material6];
+      mesh = new THREE.Mesh(geometry, mats);
+      mesh.scale.set(0.3, 0.3, 0.3);
+      mesh.position.set(0, -9, 0);
+      scene.add(mesh);
+    });
+  }
 }
 
 function rotateModel() {
-	if(!mesh){
-		return;
-	}
-	mesh.rotation.y += 0.015*mousePos.x;
+  if (!mesh) {
+    return;
+  }
+  mesh.rotation.y += 0.015 * mousePos.x;
 };
 
 function render() {
-	requestAnimationFrame(render);
-	rotateModel();
-	renderer.render(scene, camera);
+  requestAnimationFrame(render);
+  rotateModel();
+  renderer.render(scene, camera);
 }
 
 function initLights() {
-	var light = new THREE.AmbientLight(0xffffff);
-	scene.add(light);
+  var light = new THREE.AmbientLight(0xffffff);
+  scene.add(light);
 }
 
-function removeEntity(){
-    scene.remove(mesh);
+function removeEntity() {
+  scene.remove(mesh);
 }
 
-function changeModel() {
-	select++;
-	removeEntity();
+function changeModel(direction) {
+  console.log(direction);
+  if (direction == "left") {
+    if (select < 3) {
+			//rightBtn.style.visibility = true;
+      select++;
+    } else {
+			leftBtn.style.visibility = false;
+		}
+    //console.log("left: " + select);
+  }
 
-	if(select%3 == 0) {
-		american = false;
-		bioAmerican.style.display = "none";
-		russian = false;
-		bioRussian.style.display = "none";
-		cuban = true;
-		bioCuban.style.display = "initial";
-	} else if(select%2 == 0) {
-		american = false;
-		bioAmerican.style.display = "none";
-		cuban = false;
-		bioCuban.style.display = "none";
-		russian = true;
-		bioRussian.style.display = "initial";
-	} else {
-		russian = false;
-		bioRussian.style.display = "none";
-		cuban = false;
-		bioCuban.style.display = "none";
-		american = true;
-		bioAmerican.style.display = "initial";
-	}
-	/*american = false;
-	cuban = false;
-	russian = true;*/
-	initModel();
+  if (direction == "right") {
+    if (select > 1) {
+			//leftBtn.style.visibility = true;
+      select--;
+    } else {
+    	rightBtn.style.visibility = false;
+    }
+    // console.log("right: " + select);
+  }
+
+  removeEntity();
+
+  if (select % 3 == 0) {
+    american = false;
+    bioAmerican.style.display = "none";
+    russian = false;
+    bioRussian.style.display = "none";
+    cuban = true;
+    bioCuban.style.display = "initial";
+  } else if (select % 2 == 0) {
+    american = false;
+    bioAmerican.style.display = "none";
+    cuban = false;
+    bioCuban.style.display = "none";
+    russian = true;
+    bioRussian.style.display = "initial";
+  } else {
+    russian = false;
+    bioRussian.style.display = "none";
+    cuban = false;
+    bioCuban.style.display = "none";
+    american = true;
+    bioAmerican.style.display = "initial";
+  }
+  /*american = false;
+  cuban = false;
+  russian = true;*/
+  initModel();
 }
--- IMY 320 Assignment/javascript/transitions.js
+++ IMY 320 Assignment/javascript/transitions.js
@@ -1,172 +1,185 @@
-
-
 $(document).ready(function() {
-	console.log("hey");
-	$('#replace').hide();
-	$('.aniTxt').textillate({out: {effect: 'fadeOutDown', synch: true}});
+  console.log("hey");
+  $('#replace').hide();
+  $('.aniTxt').textillate({
+    out: {
+      effect: 'fadeOutDown',
+      synch: true
+    }
+  });
 
 
-$("#NavHome").click(function(){
-	console.log("hey");
-		$('#replace').show();
-         /* $("body").css("background-image", "url(fronPageBackgroundOne.jpg)");
+  $("#NavHome").click(function() {
+    console.log("hey");
+    $('#replace').show();
+    /* $("body").css("background-image", "url(fronPageBackgroundOne.jpg)");
         	setTimeout(function(){ $("body").css("background-image", "url(fronPageBackground.jpg)"); },5000);*/
 
-        	/**$("body").animate({ "background-image", "url(fronPageBackground.jpg)", 2000);**/
+    /**$("body").animate({ "background-image", "url(fronPageBackground.jpg)", 2000);**/
 
 
-            $('body').fadeOut('slow', function () {
+    $('body').fadeOut('slow', function() {
 
-                $('body').css({ 'background-image': 'url(images/fronPageBackgroundOne.jpg)' });
+      $('body').css({
+        'background-image': 'url(images/fronPageBackgroundOne.jpg)'
+      });
 
-                $('body').fadeIn(3000);
-            });
+      $('body').fadeIn(3000);
+    });
 
-});
-
-
-$("#NavAbout").click(function(){
-   		if($('.aniTxt')) {
-			$('.aniTxt').textillate('out');
-			$('.aniTxt').removeClass('aniTxt');
-		}
-         $('body').fadeOut('slow', function () {
-
-                $('body').css({ 'background-image': 'url(images/missile2.jpg)' });
-
-                $('body').fadeIn(3000);
-            });
-
-        	/**$("body").animate({ "background-image", "url(fronPageBackground.jpg)", 2000);**/
-});
+  });
 
 
-$("#NavTeam").click(function(){
-	console.log("hey");
-        
-     	if($('.aniTxt')) {
-			$('.aniTxt').textillate('out');
-			$('.aniTxt').removeClass('aniTxt');
-		}
+  $("#NavAbout").click(function() {
+    if ($('.aniTxt')) {
+      $('.aniTxt').textillate('out');
+      $('.aniTxt').removeClass('aniTxt');
+    }
+    $('body').fadeOut('slow', function() {
 
-        	/**$("body").animate({ "background-image", "url(fronPageBackground.jpg)", 2000);**/
+      $('body').css({
+        'background-image': 'url(images/missile2.jpg)'
+      });
 
+      $('body').fadeIn(3000);
+    });
 
-         $('body').fadeOut('slow', function () {
-
-                $('body').css({ 'background-image': 'url(images/smallBoy2.jpg)' });
-
-                $('body').fadeIn(3000);
-            });
-});
-
-$("#NavReviews").click(function(){
-	console.log("hey");
-        
-
-		if($('.aniTxt')) {
-			$('.aniTxt').textillate('out');
-			$('.aniTxt').removeClass('aniTxt');
-		}
-        	 
+    /**$("body").animate({ "background-image", "url(fronPageBackground.jpg)", 2000);**/
+  });
 
 
-         $('body').fadeOut('slow', function () {
+  $("#NavTeam").click(function() {
+    console.log("hey");
 
-                $('body').css({ 'background-image': 'url(images/micro2.jpg)' });
+    if ($('.aniTxt')) {
+      $('.aniTxt').textillate('out');
+      $('.aniTxt').removeClass('aniTxt');
+    }
 
-                $('body').fadeIn(3000);
-            });
-});
+    /**$("body").animate({ "background-image", "url(fronPageBackground.jpg)", 2000);**/
 
 
-$("#NavGallery").click(function(){
-	console.log("hey");
-        if($('.aniTxt')) {
-			$('.aniTxt').textillate('out');
-			$('.aniTxt').removeClass('aniTxt');
-		}
-       
-		$('.aniTxt').textillate('out');
+    $('body').fadeOut('slow', function() {
 
-         $('body').fadeOut('slow', function () {
+      $('body').css({
+        'background-image': 'url(images/smallBoy2.jpg)'
+      });
 
-                $('body').css({ 'background-image': 'url(images/koreanCOld2.jpg)' });
+      $('body').fadeIn(3000);
+    });
+  });
 
-                $('body').fadeIn(3000);
-            });
-});
+  $("#NavReviews").click(function() {
+    console.log("hey");
+
+
+    if ($('.aniTxt')) {
+      $('.aniTxt').textillate('out');
+      $('.aniTxt').removeClass('aniTxt');
+    }
+
+
+
+    $('body').fadeOut('slow', function() {
+
+      $('body').css({
+        'background-image': 'url(images/micro2.jpg)'
+      });
+
+      $('body').fadeIn(3000);
+    });
+  });
+
+
+  $("#NavGallery").click(function() {
+    console.log("hey");
+    if ($('.aniTxt')) {
+      $('.aniTxt').textillate('out');
+      $('.aniTxt').removeClass('aniTxt');
+    }
+
+    $('.aniTxt').textillate('out');
+
+    $('body').fadeOut('slow', function() {
+
+      $('body').css({
+        'background-image': 'url(images/koreanCOld2.jpg)'
+      });
+
+      $('body').fadeIn(3000);
+    });
+  });
 });
 
 
 var unselectedColor = '#848484';
 
 function showTeam() {
-	document.body.style.backgroundImage = "url('images/fronPageBackground.jpg')";
-    document.getElementById('Team').style.visibility = 'visible';
-	document.getElementById('NavTeam').style.color = '#ffffff';
-	document.getElementById('Home').style.visibility = 'hidden';
-	document.getElementById('NavHome').style.color = unselectedColor;
-	document.getElementById('Gallery').style.visibility = 'hidden';
-	document.getElementById('NavGallery').style.color = unselectedColor;
-	document.getElementById('About').style.visibility = 'hidden';
-	document.getElementById('NavAbout').style.color = unselectedColor;
-	document.getElementById('Reviews').style.visibility = 'hidden';
-	document.getElementById('NavReviews').style.color = unselectedColor;
+  document.body.style.backgroundImage = "url('images/fronPageBackground.jpg')";
+  document.getElementById('Team').style.visibility = 'visible';
+  document.getElementById('NavTeam').style.color = '#ffffff';
+  document.getElementById('Home').style.visibility = 'hidden';
+  document.getElementById('NavHome').style.color = unselectedColor;
+  document.getElementById('Gallery').style.visibility = 'hidden';
+  document.getElementById('NavGallery').style.color = unselectedColor;
+  document.getElementById('About').style.visibility = 'hidden';
+  document.getElementById('NavAbout').style.color = unselectedColor;
+  document.getElementById('Reviews').style.visibility = 'hidden';
+  document.getElementById('NavReviews').style.color = unselectedColor;
 }
 
 function showHome() {
-	document.body.style.backgroundImage = "url('images/fronPageBackground.jpg')";
-    document.getElementById('Team').style.visibility = 'hidden';
-	document.getElementById('NavTeam').style.color = unselectedColor;
-	document.getElementById('Home').style.visibility = 'visible';
-	document.getElementById('NavHome').style.color = '#ffffff';
-	document.getElementById('Gallery').style.visibility = 'hidden';
-	document.getElementById('NavGallery').style.color = unselectedColor;
-	document.getElementById('About').style.visibility = 'hidden';
-	document.getElementById('NavAbout').style.color = unselectedColor;
-	document.getElementById('Reviews').style.visibility = 'hidden';
-	document.getElementById('NavReviews').style.color = unselectedColor;
+  document.body.style.backgroundImage = "url('images/fronPageBackground.jpg')";
+  document.getElementById('Team').style.visibility = 'hidden';
+  document.getElementById('NavTeam').style.color = unselectedColor;
+  document.getElementById('Home').style.visibility = 'visible';
+  document.getElementById('NavHome').style.color = '#ffffff';
+  document.getElementById('Gallery').style.visibility = 'hidden';
+  document.getElementById('NavGallery').style.color = unselectedColor;
+  document.getElementById('About').style.visibility = 'hidden';
+  document.getElementById('NavAbout').style.color = unselectedColor;
+  document.getElementById('Reviews').style.visibility = 'hidden';
+  document.getElementById('NavReviews').style.color = unselectedColor;
 }
 
 function showAbout() {
-	document.body.style.backgroundImage = "url('images/fronPageBackground.jpg')";
-    document.getElementById('Team').style.visibility = 'hidden';
-	document.getElementById('NavTeam').style.color = unselectedColor;
-	document.getElementById('Home').style.visibility = 'hidden';
-	document.getElementById('NavHome').style.color = unselectedColor;
-	document.getElementById('Gallery').style.visibility = 'hidden';
-	document.getElementById('NavGallery').style.color = unselectedColor;
-	document.getElementById('About').style.visibility = 'visible';
-	document.getElementById('NavAbout').style.color = '#ffffff';
-	document.getElementById('Reviews').style.visibility = 'hidden';
-	document.getElementById('NavReviews').style.color = unselectedColor;
+  document.body.style.backgroundImage = "url('images/fronPageBackground.jpg')";
+  document.getElementById('Team').style.visibility = 'hidden';
+  document.getElementById('NavTeam').style.color = unselectedColor;
+  document.getElementById('Home').style.visibility = 'hidden';
+  document.getElementById('NavHome').style.color = unselectedColor;
+  document.getElementById('Gallery').style.visibility = 'hidden';
+  document.getElementById('NavGallery').style.color = unselectedColor;
+  document.getElementById('About').style.visibility = 'visible';
+  document.getElementById('NavAbout').style.color = '#ffffff';
+  document.getElementById('Reviews').style.visibility = 'hidden';
+  document.getElementById('NavReviews').style.color = unselectedColor;
 }
 
 function showGallery() {
-	document.body.style.backgroundImage = "url('images/fronPageBackground.jpg')";
-    document.getElementById('Team').style.visibility = 'hidden';
-	document.getElementById('NavTeam').style.color = unselectedColor;
-	document.getElementById('Home').style.visibility = 'hidden';
-	document.getElementById('NavHome').style.color = unselectedColor;
-	document.getElementById('Gallery').style.visibility = 'visible';
-	document.getElementById('NavGallery').style.color = '#ffffff';
-	document.getElementById('About').style.visibility = 'hidden';
-	document.getElementById('NavAbout').style.color = unselectedColor;
-	document.getElementById('Reviews').style.visibility = 'hidden';
-	document.getElementById('NavReviews').style.color = unselectedColor;
+  document.body.style.backgroundImage = "url('images/fronPageBackground.jpg')";
+  document.getElementById('Team').style.visibility = 'hidden';
+  document.getElementById('NavTeam').style.color = unselectedColor;
+  document.getElementById('Home').style.visibility = 'hidden';
+  document.getElementById('NavHome').style.color = unselectedColor;
+  document.getElementById('Gallery').style.visibility = 'visible';
+  document.getElementById('NavGallery').style.color = '#ffffff';
+  document.getElementById('About').style.visibility = 'hidden';
+  document.getElementById('NavAbout').style.color = unselectedColor;
+  document.getElementById('Reviews').style.visibility = 'hidden';
+  document.getElementById('NavReviews').style.color = unselectedColor;
 }
 
 function showReviews() {
-	document.body.style.backgroundImage = "url('images/fronPageBackground.jpg')";
-    document.getElementById('Team').style.visibility = 'hidden';
-	document.getElementById('NavTeam').style.color = unselectedColor;
-	document.getElementById('Home').style.visibility = 'hidden';
-	document.getElementById('NavHome').style.color = unselectedColor;
-	document.getElementById('Gallery').style.visibility = 'hidden';
-	document.getElementById('NavGallery').style.color = unselectedColor;
-	document.getElementById('About').style.visibility = 'hidden';
-	document.getElementById('NavAbout').style.color = unselectedColor;
-	document.getElementById('Reviews').style.visibility = 'visible';
-	document.getElementById('NavReviews').style.color = '#ffffff';
+  document.body.style.backgroundImage = "url('images/fronPageBackground.jpg')";
+  document.getElementById('Team').style.visibility = 'hidden';
+  document.getElementById('NavTeam').style.color = unselectedColor;
+  document.getElementById('Home').style.visibility = 'hidden';
+  document.getElementById('NavHome').style.color = unselectedColor;
+  document.getElementById('Gallery').style.visibility = 'hidden';
+  document.getElementById('NavGallery').style.color = unselectedColor;
+  document.getElementById('About').style.visibility = 'hidden';
+  document.getElementById('NavAbout').style.color = unselectedColor;
+  document.getElementById('Reviews').style.visibility = 'visible';
+  document.getElementById('NavReviews').style.color = '#ffffff';
 }
