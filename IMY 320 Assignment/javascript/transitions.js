dfd4704a2fc7b4c162faa0b0cf70b43310493dcf
Anneta Wamono <annetawamono@yahoo.com.au>
2017-10-05 12:33:31 +0200

    YAAAASSSSSS got the easter egg working

--- IMY 320 Assignment/javascript/transitions.js
+++ IMY 320 Assignment/javascript/transitions.js
@@ -1,6 +1,6 @@
 $(function(){
 
-  //********** page transitions ***********
+  // page transitions
   $("#NavHome").click(function(){
     $("#transition-block").animate({
       height: "toggle"
@@ -37,7 +37,7 @@ $(function(){
     });
   });
 
-  //************** easter eggs ****************
+  // easter eggs
   $(".egg").mouseenter(function(){
     $("body").css('background-image', 'url(../images/fronPageBackgroundOne.jpg)');
   }).mouseleave(function(){
@@ -45,75 +45,19 @@ $(function(){
   });
 
   $(".egg").lettering();
-  //render2();
-
-  arrayFonts = ["serif", "san-serif", "256_bytesregular"];
-  c = 0;
-  setInterval (function () {
-    console.log("change font");
-    //$(".egg>span").css('font-family', arrayFonts[Math.floor(Math.random() * (3 - 0 + 1)) + 0]);
-    c++;
-    $(".egg>span").css('fontFamily', arrayFonts[c % arrayFonts.length]);
-    }, 500);
-
-    //***************** team page *********************
-    $("#anneta").hover(function(){
-      $("body").css('background-image', 'url(../images/imy320pic1.jpg)');
-      $(this).text(function(i, origText){
-        return "<" + origText + ">";
-        });
-    }, function(){
-      $("body").css('background-image', 'url(../images/koreanCOld.jpg)');
-      $(this).text(function(i, origText){
-        return origText.substring(1, origText.length-1);
-      });
-    });
-
-    $("#matthew").hover(function(){
-      $(this).text(function(i, origText){
-        return "<" + origText + ">";
-        });
-    }, function(){
-      $(this).text(function(i, origText){
-        return origText.substring(1, origText.length-1);
-      });
-    });
-
-    $("#rish").hover(function(){
-      $(this).text(function(i, origText){
-        return "<" + origText + ">";
-        });
-    }, function(){
-      $(this).text(function(i, origText){
-        return origText.substring(1, origText.length-1);
-      });
-    });
-
-    $("#dave").hover(function(){
-      $(this).text(function(i, origText){
-        return "<" + origText + ">";
-        });
-    }, function(){
-      $(this).text(function(i, origText){
-        return origText.substring(1, origText.length-1);
-      });
-    });
-
-    $("#anneta").click(function(){
-          $("body").css('background-image', 'url(../images/imy320pic1.jpg)');
-    });
+  render2();
 });
 
-//var arrayFonts = ["serif", "san-serif", "256_bytesregular"];
+var arrayFonts = ["\'serif\'", "\'san-serif\'", "\'256_bytesregular\'"];
 
 // TODO: fix this
-// function render2() {
-//   $(".egg>span").each(function(){
-//     console.log("render2");
-//     $(this).css('font-family', arrayFonts[Math.floor(Math.random() * (3 - 0 + 1)) + 0]);
-//   });
-//   t = setTimeout(function(){ render2() }, 500);
-// }
+function render2() {
+  $(".egg>span").each(function(){
+    console.log("render2");
+    $(this).css('font-family', arrayFonts[Math.floor(Math.random() * (3 - 0 + 1)) + 0]);
+  });
+  t = setTimeout(function(){ render2() }, 500);
+}
 
 var unselectedColor = '#848484';
 // var home = document.getElementById('Home');
@@ -126,19 +70,19 @@ var unselectedColor = '#848484';
 // var teamNav = document.getElementById('NavTeam');
 
 function showTeam() {
-  document.body.style.backgroundImage = "url('images/koreanCOld.jpg')";
+  document.body.style.backgroundImage = "url('images/fronPageBackground.jpg')";
   document.getElementById('Home').style.visibility = 'hidden';
   document.getElementById('NavHome').style.color = unselectedColor;
-  // document.getElementById('NavHome').style.fontSize = '30px';
+  document.getElementById('NavHome').style.fontSize = '30px';
   document.getElementById('Gallery').style.visibility = 'hidden';
   document.getElementById('NavGallery').style.color = unselectedColor;
-  // document.getElementById('NavGallery').style.fontSize = '30px';
+  document.getElementById('NavGallery').style.fontSize = '30px';
   document.getElementById('About').style.visibility = 'hidden';
   document.getElementById('NavAbout').style.color = unselectedColor;
-  // document.getElementById('NavAbout').style.fontSize = '30px';
+  document.getElementById('NavAbout').style.fontSize = '30px';
   document.getElementById('Team').style.visibility = 'visible';
   document.getElementById('NavTeam').style.color = '#ffffff';
-  // document.getElementById('NavTeam').style.fontSize = '36px';
+  document.getElementById('NavTeam').style.fontSize = '36px';
   document.getElementById('Reviews').style.visibility = 'hidden';
 }
 
@@ -146,50 +90,50 @@ function showHome() {
   document.body.style.backgroundImage = "url('images/fronPageBackground.jpg')";
   document.getElementById('Team').style.visibility = 'hidden';
   document.getElementById('NavTeam').style.color = unselectedColor;
-  // document.getElementById('NavTeam').style.fontSize = '30px';
+  document.getElementById('NavTeam').style.fontSize = '30px';
   document.getElementById('Home').style.visibility = 'visible';
   document.getElementById('NavHome').style.color = '#ffffff';
-  // document.getElementById('NavHome').style.fontSize = '36px';
+  document.getElementById('NavHome').style.fontSize = '36px';
   document.getElementById('Gallery').style.visibility = 'hidden';
   document.getElementById('NavGallery').style.color = unselectedColor;
-  // document.getElementById('NavGallery').style.fontSize = '30px';
+  document.getElementById('NavGallery').style.fontSize = '30px';
   document.getElementById('About').style.visibility = 'hidden';
   document.getElementById('NavAbout').style.color = unselectedColor;
-  // document.getElementById('NavAbout').style.fontSize = '30px';
+  document.getElementById('NavAbout').style.fontSize = '30px';
   document.getElementById('Reviews').style.visibility = 'hidden';
 }
 
 function showAbout() {
-  document.body.style.backgroundImage = "url('images/missile.jpg')";
+  document.body.style.backgroundImage = "url('images/fronPageBackground.jpg')";
   document.getElementById('Team').style.visibility = 'hidden';
   document.getElementById('NavTeam').style.color = unselectedColor;
-  // document.getElementById('NavTeam').style.fontSize = '30px';
+  document.getElementById('NavTeam').style.fontSize = '30px';
   document.getElementById('Home').style.visibility = 'hidden';
   document.getElementById('NavHome').style.color = unselectedColor;
-  // document.getElementById('NavHome').style.fontSize = '30px';
+  document.getElementById('NavHome').style.fontSize = '30px';
   document.getElementById('Gallery').style.visibility = 'hidden';
   document.getElementById('NavGallery').style.color = unselectedColor;
-  // document.getElementById('NavGallery').style.fontSize = '30px';
+  document.getElementById('NavGallery').style.fontSize = '30px';
   document.getElementById('About').style.visibility = 'visible';
   document.getElementById('NavAbout').style.color = '#ffffff';
-  // document.getElementById('NavAbout').style.fontSize = '36px';
+  document.getElementById('NavAbout').style.fontSize = '36px';
   document.getElementById('Reviews').style.visibility = 'hidden';
 }
 
 function showGallery() {
-  document.body.style.backgroundImage = "url('images/smallBoy.jpg')";
+  document.body.style.backgroundImage = "url('images/fronPageBackground.jpg')";
   document.getElementById('Team').style.visibility = 'hidden';
   document.getElementById('NavTeam').style.color = unselectedColor;
-  // document.getElementById('NavTeam').style.fontSize = '30px';
+  document.getElementById('NavTeam').style.fontSize = '30px';
   document.getElementById('Home').style.visibility = 'hidden';
   document.getElementById('NavHome').style.color = unselectedColor;
-  // document.getElementById('NavHome').style.fontSize = '30px';
+  document.getElementById('NavHome').style.fontSize = '30px';
   document.getElementById('Gallery').style.visibility = 'visible';
   document.getElementById('NavGallery').style.color = '#ffffff';
-  // document.getElementById('NavGallery').style.fontSize = '36px';
+  document.getElementById('NavGallery').style.fontSize = '36px';
   document.getElementById('About').style.visibility = 'hidden';
   document.getElementById('NavAbout').style.color = unselectedColor;
-  // document.getElementById('NavAbout').style.fontSize = '30px';
+  document.getElementById('NavAbout').style.fontSize = '30px';
   document.getElementById('Reviews').style.visibility = 'hidden';
 }
 
